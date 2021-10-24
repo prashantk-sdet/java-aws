969a11b1e705e5838f76edbdebdfeaadd125ede0
Prashant Kumar <prashant.jiit@gmail.com>
2021-10-24 23:58:31 +0530

    Initial commit

---
+++ src/main/java/s3/ListBuckets.java
@@ -0,0 +1,29 @@
+package s3;
+
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.profile.ProfileCredentialsProvider;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.Bucket;
+
+import java.util.List;
+
+/**
+ * List your Amazon S3 buckets.
+ * 
+ * This code expects that you have AWS credentials set up per:
+ * http://docs.aws.amazon.com/java-sdk/latest/developer-guide/setup-credentials.html
+ */
+public class ListBuckets {
+    public static void main(String[] args) {
+    	AWSCredentials credentials = new ProfileCredentialsProvider().getCredentials();
+    	
+        final AmazonS3 s3 = AmazonS3ClientBuilder.standard().withRegion(Regions.DEFAULT_REGION).build();
+        List<Bucket> buckets = s3.listBuckets();
+        System.out.println("Your Amazon S3 buckets are:");
+        for (Bucket b : buckets) {
+            System.out.println("* " + b.getName());
+        }
+    }
+}
